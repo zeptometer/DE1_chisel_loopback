2028e4305b39e4f052f37a45e323c4e62a332854
zeptometer <yuito.murase@gmail.com>
2015-07-07 23:17:15 +0900

    UartTx worked

--- DE1_Uart_test.v
+++ DE1_Uart_test.v
@@ -4,47 +4,46 @@
 //=======================================================
 
 module DE1_Uart_test(
+		     //////////// CLOCK //////////
+		     input 	  CLOCK_50,
+		     input 	  CLOCK2_50,
+		     input 	  CLOCK3_50,
+		     input 	  CLOCK4_50,
 
-	//////////// CLOCK //////////
-	input 		          		CLOCK_50,
-	input 		          		CLOCK2_50,
-	input 		          		CLOCK3_50,
-	input 		          		CLOCK4_50,
+		     //////////// SEG7 //////////
+		     output [6:0] HEX0,
+		     output [6:0] HEX1,
+		     output [6:0] HEX2,
+		     output [6:0] HEX3,
+		     output [6:0] HEX4,
+		     output [6:0] HEX5,
 
-	//////////// SEG7 //////////
-	output		     [6:0]		HEX0,
-	output		     [6:0]		HEX1,
-	output		     [6:0]		HEX2,
-	output		     [6:0]		HEX3,
-	output		     [6:0]		HEX4,
-	output		     [6:0]		HEX5,
+		     //////////// KEY //////////
+		     input [3:0]  KEY,
 
-	//////////// KEY //////////
-	input 		     [3:0]		KEY,
+		     //////////// LED //////////
+		     output [9:0] LEDR,
 
-	//////////// LED //////////
-	output		     [9:0]		LEDR,
+		     //////////// SW //////////
+		     input [9:0]  SW,
 
-	//////////// SW //////////
-	input 		     [9:0]		SW,
+		     //////////// GPIO_0, GPIO_0 connect to GPIO Default //////////
+		     inout [35:0] GPIO
+		     );
 
-	//////////// GPIO_0, GPIO_0 connect to GPIO Default //////////
-	inout 		    [35:0]		GPIO
-);
+   
+   wire 			  ready;
+   wire 			  valid;
+   wire[7:0] 			  data;
+   
+   assign valid = 1'b1;
+   assign data  = 8'h30;
 
-
-
-//=======================================================
-//  REG/WIRE declarations
-//=======================================================
-
-
-
-
-//=======================================================
-//  Structural coding
-//=======================================================
-
-
+   UartTx sender(.clk(CLOCK_50),
+		 .reset(~KEY[0]),
+		 .io_txd(GPIO[0]),
+		 .io_enq_ready(ready),
+		 .io_enq_valid(valid),
+		 .io_enq_bits(data));
 
 endmodule
